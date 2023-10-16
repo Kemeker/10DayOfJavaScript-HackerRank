c62407f409bf4f70b432023d7f8dda59bcc9e234
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-16 13:37:31 -0300

    add pastas do dia 01 e 02

---
+++ 2DayOfCodeJS/loopVogais.js
@@ -0,0 +1,12 @@
+function vowelsAndConsonants(s) {
+    var vowels = ''
+    var consonants = ''
+
+    for (var i = 0; i < s.length; ++i){
+        var char = s[i].toLowerCase()
+
+    }
+    
+
+    
+}
