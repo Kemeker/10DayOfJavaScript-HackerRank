e6b437b9dd4b5fc31d939175cf61c30139ca96a0
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-16 17:12:43 -0300

    dia 02 ok

--- 2DayOfCodeJS/loopVogais.js
+++ 2DayOfCodeJS/loopVogais.js
@@ -10,3 +10,32 @@ function vowelsAndConsonants(s) {
 
     
 }
+            
+function diagonalDifference(arr) {
+    const matriz = arr;
+    const tamanho = matriz.length;
+    let somaDiagonalPrincipal = 0;
+    let somaDiagonalSecundaria = 0;
+  
+    for (let i = 0; i < tamanho; i++) {
+      // Soma dos elementos da diagonal principal
+      somaDiagonalPrincipal += matriz[i][i];
+  
+      // Soma dos elementos da diagonal secundária
+      somaDiagonalSecundaria += matriz[i][tamanho - 1 - i];
+    }
+  
+    const diferenca = Math.abs(somaDiagonalPrincipal - somaDiagonalSecundaria);
+    console.log(diferenca);
+  }
+  
+  // Exemplo de uso com a matriz fornecida
+  const matriz = [
+    [3],
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+  ];
+  
+  diagonalDifference(matriz);
+  
