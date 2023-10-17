9b5fa611130e8648c8cb5530900b972aca5bf81b
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-17 17:21:21 -0300

    teste

---
+++ 1DayOfCodeJS/1DayOfCodeJS-Let&Const.js
@@ -0,0 +1,10 @@
+function main(){
+const pi = Math.PI
+const raio = parseFloat(redline())
+
+const area = pi * raio * raio
+const perimeter = 2 * pi * area
+
+console.log(area)
+console.log(perimeter)
+}
