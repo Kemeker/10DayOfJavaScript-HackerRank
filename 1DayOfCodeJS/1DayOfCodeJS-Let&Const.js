e10643c85625c75d4719df0cf4c38992d8b98546
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-18 13:54:33 -0300

    correçao do dia 1

--- 1DayOfCodeJS/1DayOfCodeJS-Let&Const.js
+++ 1DayOfCodeJS/1DayOfCodeJS-Let&Const.js
@@ -1,10 +1,21 @@
-function main(){
 const pi = Math.PI
-const raio = parseFloat(redline())
+function main() {
+    
+    const r = parseFloat(readLine())
 
-const area = pi * raio * raio
-const perimeter = 2 * pi * area
+    const area = pi * r * r
+    const perimeter = (2 * pi)* r
 
-console.log(area)
-console.log(perimeter)
+    console.log(area)
+    console.log(perimeter)
+    
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
 }
