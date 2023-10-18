e6970f8ecebd5440fc251b3045cfec110b30d4d0
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-18 16:05:00 -0300

    resolvido query de codicionais com um novo codigo

--- 2DayOfCodeJS/2DayOfCodeJS-Condicional.js
+++ 2DayOfCodeJS/2DayOfCodeJS-Condicional.js
@@ -1,20 +1,21 @@
 function getGrade(score) {
-    let grade = "D"
-    if (25 < score || 30 < score ){
-        return grade
-    }
-    if(20 < score || 25 < score){
-            return grade
-            
-    }
-    if(15 < score || 20 < score){
-        return grade
-    }
-    if(10< score || 15 < score){
-        return grade
+    let grade;
+    
+    if (score > 25 && score <= 30) {
+        grade = "A";
+    } else if (score > 20 && score <= 25) {
+        grade = "B";
+    } else if (score > 15 && score <= 20) {
+        grade = "C";
+    } else if (score >= 10 && score < 15) {
+        grade = "D";
+    } else if (score >= 5 && score < 10) {
+        grade = "E";
+    } else if (score >= 0 && score < 5) {
+        grade = "F";
     }
     
     return grade;
-    
-    
 }
+    
+    
