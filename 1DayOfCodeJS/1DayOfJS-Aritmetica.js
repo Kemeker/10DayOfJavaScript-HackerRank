86b6f0e680f9af1f9f23a58496be7f746998cc22
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-18 13:55:17 -0300

    att nos arquivos

--- 1DayOfCodeJS/1DayOfJS-Aritmetica.js
+++ 1DayOfCodeJS/1DayOfJS-Aritmetica.js
@@ -1,13 +1,21 @@
-function getArea(length, width) {
-    let area;
-    area = length * width
-    return area;
-}  
-
-
-function getPerimeter(length, width) {
-    let perimeter;
-    perimeter = 2 * (length + width)
+const pi = Math.PI
+function main() {
     
-    return perimeter;
-}   
+    const r = parseFloat(readLine())
+
+    const area = pi * r * r
+    const perimeter = (2 * pi)* r
+
+    console.log(area)
+    console.log(perimeter)
+    
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
