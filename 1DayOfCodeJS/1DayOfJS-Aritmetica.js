9eee55feccfbd3cdba1f94462ac9e6a5440b91fd
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-18 13:57:27 -0300

    atualizando os codigos do day 1 de codigo, agora proseguindo ao dia 2

--- 1DayOfCodeJS/1DayOfJS-Aritmetica.js
+++ 1DayOfCodeJS/1DayOfJS-Aritmetica.js
@@ -1,21 +1,20 @@
-const pi = Math.PI
-function main() {
+function getArea(length, width) {
+    let area;
+    area = length * width
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = 2 * (length + width)
     
-    const r = parseFloat(readLine())
-
-    const area = pi * r * r
-    const perimeter = (2 * pi)* r
-
-    console.log(area)
-    console.log(perimeter)
-    
-
-    try {    
-        // Attempt to redefine the value of constant variable PI
-        PI = 0;
-        // Attempt to print the value of PI
-        console.log(PI);
-    } catch(error) {
-        console.error("You correctly declared 'PI' as a constant.");
-    }
+    return perimeter;
 }
