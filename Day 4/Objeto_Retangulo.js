78a8d270b40358a51457afc3b039f438a899eae5
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-20 14:04:29 -0300

    resolvido a questao de objeto, desenvolvido uma logica para usar um objeto com varios parametros

---
+++ Day 4/Objeto_Retangulo.js
@@ -0,0 +1,14 @@
+function rectangle(a, b ){
+    const perimeter = 2 * (a + b)
+    const area = a * b
+    const Object = {
+        length: a,
+        width: b, 
+        perimeter: perimeter,
+        area: area
+    }
+    return Object
+
+    
+
+}
